9c7dd4c2c79fa1e13d47297eb73b40e69d0dd8fd
RomainMechain <romain.mechain@etu.univ-orleans.fr>
2024-01-12 14:39:46 +0100

    Fix/ Correction de la mise en place de l'URL dans le JS

--- app/static/JS/search_event.js
+++ app/static/JS/search_event.js
@@ -1,5 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     let select_type = document.getElementById('select_type');
+    let params = new URLSearchParams(window.location.search);
+    let id_type = params.get('id_type') || '';
+    let search_term = params.get('search_term') || '';
 
     function loadSelection() {
         let type = JSON.parse(localStorage.getItem('id_type')) || 'all';
@@ -9,11 +12,7 @@ document.addEventListener('DOMContentLoaded', function() {
     select_type.addEventListener('change', function() {
         let type = select_type.value;
         let url = window.location.href.split('?')[0] + '?';
-        url += 'id_type=' + type + '&';
-        let search_term = JSON.parse(localStorage.getItem('search_term')) || '';
-        if (search_term != '') {
-            url += 'search_term=' + search_term + '&';
-        }
+        url += 'id_type=' + type + '&' + 'search_term=' + search_term + '&';
 
         window.location.href = url;
 
@@ -25,11 +24,8 @@ document.addEventListener('DOMContentLoaded', function() {
         if (e.key === 'Enter') {
           e.preventDefault(); 
           var searchValue = e.target.value;
-          let url = window.location.href;
-          if (! url.includes('id_type')) {
-            url = url + "?";
-          }
-          url += 'search_term=' + searchValue + '&';
+          let url = window.location.href.split('?')[0] + '?';
+          url += 'id_type=' + id_type + '&' + 'search_term=' + searchValue + '&';
 
           localStorage.setItem('search_term', JSON.stringify(searchValue));
 
@@ -40,9 +36,4 @@ document.addEventListener('DOMContentLoaded', function() {
     window.onload = function() {
         loadSelection();
     };
-
-    window.addEventListener('beforeunload', function(e) {
-        localStorage.removeItem('search_term');
-        localStorage.removeItem('id_type');
-    });
  });
