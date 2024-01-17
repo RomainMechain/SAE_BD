0b5c3e8b6392cbf94c7895ce221bb03fae708ed9
RomainMechain <romain.mechain@etu.univ-orleans.fr>
2024-01-17 21:05:16 +0100

    Feat/ ajout du formulaire pour l'ajout de groupe

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, HiddenField, FileField, DateTimeField, FloatField, SelectField
+from wtforms import StringField, PasswordField, SubmitField, HiddenField, FileField, DateTimeField, FloatField, SelectField, SelectMultipleField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
@@ -39,3 +39,20 @@ class addEventForm(FlaskForm) :
         self.groupe.choices = [(groupe.idGroupe, groupe.nomGroupe) for groupe in groupes]
         self.lieu.choices = [(lieu.idLieu, lieu.nomLieu) for lieu in lieux]
         self.type_event.choices = [(type_event.idTypeEvenement, type_event.nomTypeEvenement) for type_event in types_event]
+
+class AddGroupForm(FlaskForm):
+    nom = StringField(validators=[DataRequired()], render_kw={"placeholder": "Nom"})
+    description = StringField(validators=[DataRequired()], render_kw={"placeholder": "Description"})
+    lienReseaux = StringField(validators=[DataRequired()], render_kw={"placeholder": "LienReseaux"})
+    lienVideo = StringField(validators=[DataRequired()], render_kw={"placeholder": "LienVideo"})
+    photo = FileField(validators=[DataRequired()])
+    artistes = SelectMultipleField('Artistes', coerce=int, validators=[DataRequired()])
+    instruments = SelectMultipleField('Instruments', coerce=int, validators=[DataRequired()])
+    types_musique = SelectMultipleField('TypeMusique', coerce=int, validators=[DataRequired()])
+    submit = SubmitField('Ajouter')
+
+    def __init__(self, artistes, instruments, types_musique, *args, **kwargs):
+        super(AddGroupForm, self).__init__(*args, **kwargs)
+        self.artistes.choices = [(artiste.idArtiste, artiste.nomArtiste) for artiste in artistes]
+        self.instruments.choices = [(instrument.idInstrument, instrument.nomInstrument) for instrument in instruments]
+        self.types_musique.choices = [(type_musique.idTypeMusique, type_musique.nomTypeMusique) for type_musique in types_musique]
