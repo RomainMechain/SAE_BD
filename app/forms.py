bfb2274d10052de27ba4df65cce433fa4ccd2504
RomainMechain <romain.mechain@etu.univ-orleans.fr>
2024-01-17 19:09:34 +0100

    Feat/ Ajout du formulaire pour l'ajout d'evenement

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, HiddenField, FileField
+from wtforms import StringField, PasswordField, SubmitField, HiddenField, FileField, DateTimeField, FloatField, SelectField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
@@ -22,3 +22,20 @@ class addArtisteForm(FlaskForm) :
     description = StringField(validators=[DataRequired()], render_kw={"placeholder": "Description"})
     photo = FileField(validators=[DataRequired()])
     submit = SubmitField('Ajouter')
+
+class addEventForm(FlaskForm) :
+    nom = StringField(validators=[DataRequired()], render_kw={"placeholder": "Nom"})
+    date = StringField(validators=[DataRequired()], render_kw={"placeholder": "date"})
+    duree = StringField(validators=[DataRequired()], render_kw={"placeholder": "duree"})
+    dureeMontage = StringField(validators=[DataRequired()], render_kw={"placeholder": "dureeMontage"})
+    dureeDemontage = StringField(validators=[DataRequired()], render_kw={"placeholder": "dureeDemontage"})
+    groupe = SelectField('Groupe', coerce=int, validators=[DataRequired()])
+    lieu = SelectField('Lieu', coerce=int, validators=[DataRequired()])
+    type_event = SelectField('Type', coerce=int, validators=[DataRequired()])
+    submit = SubmitField('Ajouter')
+
+    def __init__(self, groupes, lieux, types_event, *args, **kwargs):
+        super(addEventForm, self).__init__(*args, **kwargs)
+        self.groupe.choices = [(groupe.idGroupe, groupe.nomGroupe) for groupe in groupes]
+        self.lieu.choices = [(lieu.idLieu, lieu.nomLieu) for lieu in lieux]
+        self.type_event.choices = [(type_event.idTypeEvenement, type_event.nomTypeEvenement) for type_event in types_event]
